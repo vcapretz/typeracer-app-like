9e4445d836af3c57ff45c40636991508912b7637
Vitor Capretz <capretzvitor@gmail.com>
2017-09-16 00:56:31 -0300

    Add status endpoint
    it returns some info about a room, some data were mocked as we don't have them yet
    
    the user joined room event was put inside the join room listener

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const faker = require('faker');
+const moment = require('moment');
 
 const app = express();
 
@@ -13,6 +14,7 @@ const server = app.listen(3000, () => {
 const io = require('socket.io')(server);
 
 const roomsInfo = {};
+const maxWordsPerGame = 100;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('static'));
@@ -26,6 +28,7 @@ function onConnection(socket) {
         }
 
         socket.join(roomname);
+        io.to(roomname).emit('user entered room', roomsInfo[roomname]);
     });
 
     socket.on('disconnect', () => {
@@ -46,8 +49,9 @@ app.get('/room/:roomname/user/:username', (req, res) => {
         roomsInfo[roomname] = {
             roomname,
             users: [],
-            text: faker.random.words(Math.random() * 100),
-            ranking: []
+            text: faker.random.words(Math.random() * maxWordsPerGame),
+            ranking: [],
+            created_at: moment()
         };
     }
 
@@ -57,7 +61,26 @@ app.get('/room/:roomname/user/:username', (req, res) => {
 
     roomsInfo[roomname].users.push({ username });
     io.emit('requested join room', roomname);
-    io.to(roomname).emit('user entered room', roomsInfo[roomname]);
 
     res.json(`entered room ${roomname}`);
+});
+
+app.get('/room/:roomname/status', (req, res) => {
+    const { roomname } = req.params;
+
+    if (!roomsInfo[roomname]) {
+        return res.status(404).json(`room ${roomname} do not exists ):`);
+    }
+
+    const currentTime = moment();
+    const secondsToRoomCreated = currentTime.diff(roomsInfo[roomname].created_at, 'seconds');
+
+    res.json({
+        active_users: roomsInfo[roomname].users.length,
+        keystrokes: 1,
+        active_since: secondsToRoomCreated,
+        below_mean: 4,
+        ranking: roomsInfo[roomname].ranking,
+        last_minute_lead: 10,
+    });
 });
