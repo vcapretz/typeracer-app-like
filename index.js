c02883254340c950f104e60b42559ac2e1b33774
Vitor Capretz <capretzvitor@gmail.com>
2017-09-16 00:28:20 -0300

    Added endpoint to join room
    the endpoint should create a room if it not exists and allow user to connect to it
    
    the user will then emit a join room event and everyone already on the room will
    recieve a message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const faker = require('faker');
 
 const app = express();
 
@@ -11,11 +12,17 @@ const server = app.listen(3000, () => {
 
 const io = require('socket.io')(server);
 
+const roomsInfo = {};
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('static'));
 
 function onConnection(socket) {
     console.log('connected to socket');
+
+    socket.on('enter room', (roomname) => {
+        socket.join(roomname);
+    });
 
     socket.on('disconnect', () => {
         console.log('disconnected');
@@ -26,4 +33,27 @@ io.on('connection', onConnection);
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get('/room/:roomname/user/:username', (req, res) => {
+    const { roomname, username } = req.params;
+
+    if (!roomsInfo[roomname]) {
+        roomsInfo[roomname] = {
+            roomname,
+            users: [],
+            text: faker.random.words(Math.random() * 100),
+            ranking: []
+        };
+    }
+
+    if (roomsInfo[roomname].users.indexOf(username) !== -1) {
+        return res.status(403).json(`user ${username} already joined ${roomname}`);
+    }
+
+    roomsInfo[roomname].users.push({ username });
+    io.emit('requested join room', roomname);
+    io.to(roomname).emit('user entered room', roomsInfo[roomname]);
+
+    res.json(`entered room ${roomname}`);
 });
--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const socket = io();
 
-socket.on('event', (msg) => {
+socket.on('requested join room', (roomname) => {
+    socket.emit('enter room', roomname);
+});
+
+socket.on('user entered room', (msg) => {
+    if (msg.users.length >= 2) {
+        // enable button to start game
+        // the button will emit a message to room
+    }
+
     console.log(msg);
 });
